f2d7b316e832e9fa24a058f95328ff7c125ce012
ltifi <ltifisedkiinfo@gmail.com>
2020-11-11 21:19:01 +0100

    add banir

--- user/routes.js
+++ user/routes.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const UserCtrl = require('./controller/userController');
-const auth = require('../config/middleware/auth');
-const permit = require('../config/middleware/authorization');
+const UserCtrl = require("./controller/userController");
+const auth = require("../config/middleware/auth");
+const permit = require("../config/middleware/authorization");
 
-
-
-router.post('/signin', UserCtrl.login);
-router.post('/signup', UserCtrl.signup);
-router.get('/test', auth,permit('membre'),UserCtrl.test);
-router.get('/confirm/:id',UserCtrl.confirmAccount);
+router.post("/signin", UserCtrl.login);
+router.post("/signup", UserCtrl.signup);
+router.get("/test", auth, permit("membre"), UserCtrl.test);
+router.get("/confirm/:id", UserCtrl.confirmAccount);
+router.put("/update/:id", UserCtrl.update1);
 
 module.exports = router;
