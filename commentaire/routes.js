95baabd106dd4e3f6e11f837c6cd0ea9f1e49038
amal <amalbouabdallahh1@gmail.com>
2020-12-05 17:53:12 +0100

    routes and controller

---
+++ commentaire/routes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+//const commentaireCtrl = require("./controller/commentaireController");
+//const auth = require("../config/middleware/auth");
+//const permit = require("../config/middleware/authorization");
