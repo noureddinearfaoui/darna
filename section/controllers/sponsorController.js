9a9d208e4d82b925768c3cb961f3b08bdf1b4aa4
amal <amalbouabdallahh1@gmail.com>
2020-12-25 18:43:34 +0100

    sponsors

--- section/controllers/sponsorController.js
+++ section/controllers/sponsorController.js
@@ -1,18 +1,32 @@
 const Sponsor = require("../models/sponsor");
-//const fs = require("fs");
-//const directory = require("../../pathDirectory");
-//const dir = "sponsors";
-//require("dotenv").config();
+const fs = require("fs");
+const directory = require("../../pathDirectory");
+const dir = "sponsors";
+require("dotenv").config();
 
 exports.addSponsor = (req, res, next) => {
     const sponsor = new Sponsor({
         linkSponsor: req.body.linkSponsor,
-        linkPicture: req.body.linkPicture,
         });
         sponsor
           .save()
           .then(() => {
-            res.status(200).json(sponsor);
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir);
+              }
+              let files = fs.readdirSync(dir);
+              let tableOfId = [];
+              files.forEach((element) => {
+                tableOfId.push(element.split(".")[0]);
+              });
+              if (req.body.linkPicture) {
+                let data = req.body.linkPicture;
+                let buff = Buffer.from(data.split(";base64,")[1], "base64");
+                let extension = data.split(";base64,")[0].split("/")[1];
+                let fileName = dir + "/" + sponsor._id + "." + extension;
+                fs.writeFileSync(fileName, buff);
+              }
+    
           })
           .catch((error) =>
             res.status(500).json({ message: "error server" + error })
@@ -24,18 +38,38 @@ exports.updateSponsor = (req, res) => {
     const idSponsor = req.params.id;
       Sponsor.findById(idSponsor)
         .then((sponsor) => {
-          sponsor.linkSponsor = req.body.linkSponsor;
-          if(req.body.linkPicture){
-            sponsor.linkPicture = req.body.linkPicture;
-          }
+            if(req.body.newlinkSponsor){
+          sponsor.linkSponsor = req.body.newlinkSponsor;
+        }
+          sponsor.linkPicture = req.body.newlinkPicture;
           sponsor
-            .save()
-            .then(() =>{ 
-            res.status(200).json({message:"sponsor modifié avec succès"});
-             })
-            .catch((error) =>
-              res.status(500).json({ message: "Erreur serveur" + error })
-            );
+          .save()
+          .then(() =>{ 
+            if (fs.existsSync(dir)) {
+                let files = fs.readdirSync(dir);
+                if (files.find((el) => el.indexOf(sponsor._id) !== -1)) {
+                  let fileT = files.find((el) => el.indexOf(sponsor._id) !== -1);
+                  fs.unlinkSync(dir + "/" + fileT, () => {});
+                }
+              }
+              let linkPicture = "";
+          if (req.body.newlinkSponsor) {
+            let buff = Buffer.from(sponsor.linkPicture.split(";base64,")[1], "base64");
+            let extension = sponsor.linkPicture.split(";base64,")[0].split("/")[1];
+            let fileName = dir + "/" + sponsor._id + "." + extension;
+            fs.writeFileSync(fileName, buff);
+            let file = user._id + "." + extension;
+            linkPicture =
+              `${process.env.SERVER_BACKEND_ADDRESS || "http://localhost:3000"}` +
+              "/api/sponsor/app/sponsors/" +
+              file;
+          }
+          res.status(200).json({message:"Sponsor modifié avec succès"}) 
+        })
+          .catch((error) =>
+          res.status(500).json({ message: "Erreur serveur" + error })
+        );
+          
         })
         .catch((error) =>
           res.status(404).json({ message: "Sponsor non trouvé" })
