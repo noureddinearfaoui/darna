561ab8b62ad880641b19cbcaee9652114c151f24
amal <amalbouabdallahh1@gmail.com>
2020-12-25 15:25:24 +0100

    update manageHome

--- section/controllers/homeController.js
+++ section/controllers/homeController.js
@@ -5,10 +5,19 @@ exports.manageHome = (req, res) => {
   if(idHome){
     Home.findById(idHome)
     .then((home) => {
-      home.presentation=req.body.presentation;
-      home.donation=req.body.donation;
-      home.goals=req.body.goals;
-      home.announcements=req.body.announcements;
+      if(home){ 
+        if(req.body.presentation){
+          home.presentation=req.body.presentation;
+        } 
+        if(req.body.donation){
+          home.donation=req.body.donation;
+        } 
+        if(req.body.goals){
+          home.goals=req.body.goals;
+        } 
+        if(req.body.announcements){
+          home.announcements=req.body.announcements;
+        } 
       home
         .save()
         .then(() =>{ 
@@ -17,25 +26,43 @@ exports.manageHome = (req, res) => {
         .catch((error) =>
           res.status(500).json({ message: "Erreur serveur" + error })
         );
+      }
+      else{
+         res.status(404).json({ message: "Accueil non trouvé"});
+      }
     })
     .catch((error) =>
       res.status(404).json({ message: "Erreur de récupération" })
     );
+
   }
   else {
-    const hm = new Home({
-      presentation:req.body.presentation,
-      donation:req.body.donation,
-      goals:req.body.goals,
-      announcements:req.body.announcements,
-    });
-   hm.save()
-  .then(() => {
-      res.status(200).json(hm);
-    })
-    .catch((error) =>
-      res.status(500).json({ message: "erreur serveur" + error })
+    Home.find()
+    .then((homes) =>{ 
+      if (homes.length==0){
+        const hm = new Home({
+          presentation:req.body.presentation,
+          donation:req.body.donation,
+          goals:req.body.goals,
+          announcements:req.body.announcements,
+        });
+       hm.save()
+      .then(() => {
+          res.status(200).json(hm);
+        })
+        .catch((error) =>
+          res.status(500).json({ message: "erreur serveur" + error })
+        );
+      }
+      else{
+        res.status(403).json({ message: "Ajout interdit"})
+      }
+      
+       })
+       .catch((error) =>
+      res.status(500).json({ message: "Erreur serveur" })
     );
+   
   }
 };
 
