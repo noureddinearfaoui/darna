5c649bc1b0117e0b9db09022203ef2a32499e308
amal <amalbouabdallahh1@gmail.com>
2020-12-26 19:17:10 +0100

    addHelp

--- section/controllers/helpController.js
+++ section/controllers/helpController.js
@@ -1,6 +1,10 @@
 const Help= require("../models/help")
 const Link= require("../link")
-
+const directory = require("../../pathDirectory");
+const dirUploads = "uploads";
+const dir = "uploads/helps";
+const manageFiles = require("../../config/manageFiles");
+require("dotenv").config();
 
 exports.addHelp = (req, res) => {
   let answers=[];
@@ -9,7 +13,7 @@ exports.addHelp = (req, res) => {
   table.forEach(element => {
     answers.push(new Link({
       description:element.description,
-      url: element.url
+      //url: element.url
     }));
   });
   }
@@ -18,7 +22,22 @@ exports.addHelp = (req, res) => {
     answers:answers
   });
   help.save()
-    .then(() => {
+    .then((help) => {
+      help.answers.forEach((el) => { 
+    if(el.url){
+      let url =manageFiles.createFile(dirUploads,dir,el.url,el._id,
+        `${process.env.SERVER_BACKEND_ADDRESS || "http://localhost:3000"}`,
+        "/api/help/app/images/");
+      el.url=url;
+      el.save().then((resultat)=>{
+        res.status(200).json(resultat);
+      }).catch((error) =>
+        res.status(500).json({ message: error })
+      )}
+      else {
+        res.status(200).json(help);
+      }
+      });
       res.status(200).json(help);
     })
     .catch((error) =>
